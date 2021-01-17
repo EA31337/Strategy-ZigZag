ebb7720bab886fde895b6ba08a8c4f5e9490a6de
kenorb <kenorb@users.noreply.github.com>
2021-01-17 21:06:28 +0000

    Optimize Indicator params (pf=0.00; dd=0.00; tt=0)

--- config/EURUSD_M15.h
+++ config/EURUSD_M15.h
@@ -1,4 +1,4 @@
-/*
+/**
  * @file
  * Defines default strategy parameter values for the given timeframe.
  */
@@ -6,9 +6,9 @@
 // Defines indicator's parameter values for the given pair symbol and timeframe.
 struct Indi_ZigZag_Params_M15 : ZigZagParams {
   Indi_ZigZag_Params_M15() : ZigZagParams(indi_zigzag_defaults, PERIOD_M15) {
-    backstep = 3;
+    backstep = -5;
     depth = 14;
-    deviation = 5;
+    deviation = 1;
     shift = 0;
   }
 } indi_zigzag_m15;
@@ -20,12 +20,12 @@ struct Stg_ZigZag_Params_M15 : StgParams {
     lot_size = 0;
     signal_open_method = 0;
     signal_open_filter = 1;
-    signal_open_level = (float)0;
+    signal_open_level = (float)0.0;
     signal_open_boost = 0;
     signal_close_method = 0;
     signal_close_level = (float)0;
     price_stop_method = 0;
-    price_stop_level = (float)2;
+    price_stop_level = (float)1;
     tick_filter_method = 1;
     max_spread = 0;
   }
