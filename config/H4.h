1df44676d5e37ca4a95c2203afba0485e187386b
kenorb <kenorb@users.noreply.github.com>
2021-08-09 21:20:30 +0100

    Renames signal open filter param as per the latest classes change

--- config/H4.h
+++ config/H4.h
@@ -19,7 +19,6 @@ struct Stg_ZigZag_Params_H4 : StgParams {
   Stg_ZigZag_Params_H4() : StgParams(stg_zigzag_defaults) {
     lot_size = 0;
     signal_open_method = 2;
-    signal_open_filter = 32;
     signal_open_level = (float)0;
     signal_open_boost = 0;
     signal_close_method = 2;
