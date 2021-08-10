cd5139368547515878cd6f3b697f89a94bc71da5
kenorb <kenorb@users.noreply.github.com>
2021-08-10 21:16:43 +0100

    config: Enables __input__, disables __config__

--- config/define.h
+++ config/define.h
@@ -5,7 +5,7 @@
  * Uncomment a line to activate the feature.
  */
 
-#define __config__  // Loads params from the config files.
+//#define __config__  // Loads params from the config files.
 //#define __debug__        // Enables debugging.
-//#define __input__        // Enables input parameters.
+#define __input__  // Enables input parameters.
 //#define __optimize__     // Enables optimization mode.
